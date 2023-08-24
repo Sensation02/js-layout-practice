643b0bf00b947bd26de3be67ca89cd71e1356bf2
Sensation02 <vasia1415@gmail.com>
2023-08-24 20:31:10 +0300

    ToDo List

--- src/route/index.js
+++ src/route/index.js
@@ -7,12 +7,14 @@ const test = require('./test')
 // Підключіть інші файли роутів, якщо є
 const layoutPractice = require('./layout-practice')
 const community = require('./layout-practice-community')
+const todo = require('./todo')
 
 // Об'єднайте файли роутів за потреби
 router.use('/', test)
 // Використовуйте інші файли роутів, якщо є
 router.use('/layout-practice', layoutPractice)
 router.use('/layout-practice-community', community)
+router.use('/todo', todo)
 
 // Експортуємо глобальний роутер
 module.exports = router
